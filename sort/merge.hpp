58fac41f4c05f9b972a23bb117ec0efbfa89c122
Jon Anderson <jon.david.anderson@gmail.com>
2019-01-10 16:15:58 -0800

    Added merge sort.

---
+++ sort/merge.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <algorithm>
+#include <vector>
+
+namespace jander {
+
+std::vector<int> merge_sort(const std::vector<int>& unsorted) {
+  if (unsorted.size() < 2) return unsorted;
+
+  const std::vector<int>
+    unsorted1(unsorted.begin(), unsorted.begin() + unsorted.size()/2),
+    unsorted2(unsorted.begin() + unsorted.size()/2, unsorted.end());
+
+  const auto sorted1 = merge_sort(unsorted1);
+  const auto sorted2 = merge_sort(unsorted2);
+  std::vector<int> sorted;
+
+  auto iter1 = sorted1.begin();
+  auto iter2 = sorted2.begin();
+
+  while(iter1 != sorted1.end() && iter2 != sorted2.end())
+    if (*iter1 < *iter2)
+      sorted.push_back(*iter1++);
+    else
+      sorted.push_back(*iter2++);
+
+  while (iter1 != sorted1.end())
+    sorted.push_back(*iter1++);
+
+  while (iter2 != sorted2.end())
+    sorted.push_back(*iter2++);
+
+  return sorted;
+}
+
+} // namespace jander
--- sort/tests.cpp
+++ sort/tests.cpp
@@ -1,20 +1,27 @@
 #include "insert.hpp"
+#include "merge.hpp"
 #include "catch/catch.hpp"
+#include <algorithm>
+#include <array>
 
-TEST_CASE("Insert Sort (vector implementation)") {
-  std::vector<int> unsorted = {1,6,3,6,5};
-  std::vector<int> sorted = {1,3,5,6,6};
+const std::vector<int> unsorted = {9,1,6,3,6,5,8,0,12,3,2};
+const std::vector<int> sorted = {0,1,2,3,3,5,6,6,8,9,12};
+const unsigned int size = unsorted.size();
 
+TEST_CASE("Insert Sort (vector)") {
   REQUIRE(jander::insert_sort(unsorted) == sorted);
 }
 
-TEST_CASE("Insert Sort (in-place implementation)") {
-  const unsigned int size = 5;
-  int unsorted[size] = {1,6,3,6,5};
-  std::vector<int> sorted = {1,3,5,6,6};
+TEST_CASE("Insert Sort (in-place)") {
+  int unsorted_arr[size];
+  std::copy(unsorted.begin(), unsorted.end(), unsorted_arr);
 
-  jander::insert_sort(unsorted, size);
+  jander::insert_sort(unsorted_arr, size);
 
-  std::vector<int> output(unsorted, unsorted+size);
+  std::vector<int> output(unsorted_arr, unsorted_arr+size);
   REQUIRE(output == sorted);
+}
+
+TEST_CASE("Merge Sort (vector recursive)") {
+  REQUIRE(jander::merge_sort(unsorted) == sorted);
 }
