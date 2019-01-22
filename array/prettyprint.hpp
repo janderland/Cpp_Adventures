92bc08b0896e8f0a85a6ba28997a61cb032ba927
Jon Anderson <jon.david.anderson@gmail.com>
2019-01-21 18:17:56 -0800

    Switched ints to unsigned ints.

--- array/prettyprint.hpp
+++ array/prettyprint.hpp
@@ -3,14 +3,14 @@
 
 namespace jander {
 
-  std::vector<std::vector<int>> prettyprint(int A) {
-    int size = (2*A)-1;
-    std::vector<std::vector<int>> array(size);
+  std::vector<std::vector<unsigned int>> prettyprint(unsigned int A) {
+    const unsigned int size = (2*A)-1;
+    std::vector<std::vector<unsigned int>> array(size);
     for (auto& row : array) row.resize(size);
 
-    for (int i=0; i<A; i++)
-      for (int x=i; x<size-i; x++)
-        for (int y=i; y<size-i; y++)
+    for (unsigned int i=0; i<A; i++)
+      for (unsigned int x=i; x<size-i; x++)
+        for (unsigned int y=i; y<size-i; y++)
           array[x][y] = A-i;
 
     return array;
